c5f015bc5de3e5d89c0a56df0c5d9da2e5d9c884
Eduardo Socrates <edu5390@gmail.com>
2021-03-04 18:09:26 -0300

    making some adjusts to the server be up !! Finally !!!!

--- src/main/java/com/treinamento/fiapjogos/fiapjogos/Service/impl/CharacterServiceImpl.java
+++ src/main/java/com/treinamento/fiapjogos/fiapjogos/Service/impl/CharacterServiceImpl.java
@@ -9,12 +9,13 @@ import com.treinamento.fiapjogos.fiapjogos.Service.CharacterService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service
 public class CharacterServiceImpl implements CharacterService {
 
     @Autowired
--- src/main/java/com/treinamento/fiapjogos/fiapjogos/Service/impl/GameServiceImpl.java
+++ src/main/java/com/treinamento/fiapjogos/fiapjogos/Service/impl/GameServiceImpl.java
@@ -8,11 +8,12 @@ import com.treinamento.fiapjogos.fiapjogos.Service.GameService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service
 public class GameServiceImpl implements GameService {
 
     @Autowired
