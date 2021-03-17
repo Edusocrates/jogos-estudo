f50f55dc0b2bfe094dc6d6c83659e1ff762bbe7c
Eduardo Socrates <edu5390@gmail.com>
2021-03-17 17:03:17 -0300

    finishing all the DTOs tests, it's 100%

--- src/test/java/com/treinamento/fiapjogos/fiapjogos/DTO/CharacterDTOTest.java
+++ src/test/java/com/treinamento/fiapjogos/fiapjogos/DTO/CharacterDTOTest.java
@@ -1,8 +1,33 @@
 package com.treinamento.fiapjogos.fiapjogos.DTO;
 
+import com.treinamento.fiapjogos.fiapjogos.Utils.JogosUtil;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 public class CharacterDTOTest {
+
+    @Mock
+    private CharacterDTO characterDTO;
+
+    @InjectMocks
+    private JogosUtil jogosUtil;
+
+    @Test
+    void mustTestGetterAndSetter(){
+        characterDTO = jogosUtil.populateCharacterDTO();
+        assertNotNull(characterDTO);
+        assertNotNull(characterDTO.getGame());
+        assertNotNull(characterDTO.getName());
+        assertNotNull(characterDTO.getImageUrl());
+        assertNotNull(characterDTO.getId());
+    }
+
+
+
 }
